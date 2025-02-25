ee1cc3af246028420d9bda76d831a17b213f1ec1
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-02-26 03:24:02 +0530

    added wasm dependencies in react project and setup tsconfig

--- frontend/ui/vite.config.ts
+++ frontend/ui/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), wasm(), topLevelAwait()],
+  resolve: {
+    alias: {
+      "@calculator": path.resolve(__dirname, "../calculator/pkg"),
+    },
+  },
+});
